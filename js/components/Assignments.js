87bf518a3663a22bec54e2eca54e4510a34c1cf8
jmason87 <joewebdev87@gmail.com>
2022-05-30 15:01:13 -0500

    through chapters 8 and 9

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,12 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList, AssignmentCreate },
 
     template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="In Progress"></assignment-list>
+
+            <assignment-create @add="add"></assignment-create>
         </section>
     `,
     
@@ -16,7 +19,8 @@ export default {
                 { name: 'Finish Project', complete: false, id: 1 },
                 { name: 'Read Chapter 4', complete: false, id: 2 },
                 { name: 'Turn in Homework', complete: false, id: 3 },
-            ]
+            ],
+
         }
     },
     computed: {
@@ -25,6 +29,15 @@ export default {
                 inProgress: this.assignments.filter(a => ! a.complete),
                 completed: this.assignments.filter(a => a.complete)
             }
+        }
+    },
+    methods: {
+        add(name) {
+            this.assignments.push({
+                name: name,
+                completed: false,
+                id: this.assignments.length + 1
+            });
         }
     }
 
